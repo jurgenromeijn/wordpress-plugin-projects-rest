d260df6ffc24af3307e5ecf8658eb07053471650
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-03 00:36:49 +0200

    Created the project controller.

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Jurgen Romeijn <jurgen.romeijn@gmail.com>
+ */
+
+namespace JurgenRomeijn\ProjectsRest\Controller;
+
+/**
+ * The main controller which manages the Project entity.
+ * @package JurgenRomeijn\ProjectsRest\Controller
+ */
+class ProjectController extends \WP_REST_Controller
+{
+
+}
