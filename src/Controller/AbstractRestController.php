6ceee98813e1466bf47cfb2a7a0cc8c6990164a8
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-03 16:21:34 +0200

    Added simplified routing mechanism to AbstractRestController.

--- src/Controller/AbstractRestController.php
+++ src/Controller/AbstractRestController.php
@@ -5,10 +5,25 @@
 
 namespace JurgenRomeijn\ProjectsRest\Controller;
 
+use JurgenRomeijn\ProjectsRest\ProjectRestPlugin;
 use JurgenRomeijn\ProjectsRest\Util\SingletonTrait;
 use WP_REST_Controller;
 
 abstract class AbstractRestController extends WP_REST_Controller
 {
     use SingletonTrait;
+
+    const REGISTER_REST_ROUTE_METHODS  = 'methods';
+    const REGISTER_REST_ROUTE_CALLBACK = 'callback';
+
+    abstract public function init();
+
+    protected function registerRoute($route, $httpMethod, $callback)
+    {
+        $apiBasePath = ProjectRestPlugin::getApiBasePath();
+        register_rest_route($apiBasePath, $route, [
+            REGISTER_REST_ROUTE_METHODS  => $httpMethod,
+            REGISTER_REST_ROUTE_CALLBACK => [$this, $callback]
+        ]);
+    }
 }
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,10 +5,25 @@
 
 namespace JurgenRomeijn\ProjectsRest\Controller;
 
+use JurgenRomeijn\ProjectsRest\Util\HttpMethods;
+
 /**
  * The main controller which manages the Project entity.
  * @package JurgenRomeijn\ProjectsRest\Controller
  */
 class ProjectController extends AbstractRestController
 {
+    private function __construct()
+    {
+    }
+
+    public function init()
+    {
+        $this->registerRoute('/project', HttpMethods::GET, 'index');
+    }
+
+    public function index()
+    {
+        return new \WP_REST_Response('test');
+    }
 }
