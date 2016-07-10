1d8bf0962fbb9f6eb5690279818f31f1c74cc635
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-10 16:50:55 +0200

    Added an empty TestCase to add tests for the Projectcontroller.

---
+++ tests/Controller/ProjectControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Jurgen Romeijn <jurgen.romeijn@gmail.com>
+ */
+
+namespace JurgenRomeijn\ProjectsRest\Controller;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class ProjectControllerTest
+ * @package JurgenRomeijn\ProjectsRest\Controller
+ */
+class ProjectControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        // Todo: implement
+    }
+
+    public function testIndexEmpty()
+    {
+        // Todo: implement
+    }
+
+    public function testIndexNull()
+    {
+        //Todo: implement
+    }
+}
