ed1914d9f3a10edd63e3f0bb29fa63404b688c0b
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-11 16:56:04 +0200

    Added scaffolding for tests for project mapper.

---
+++ tests/Repository/Mapper/ProjectMapperTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Jurgen Romeijn <jurgen.romeijn@gmail.com>
+ */
+
+namespace JurgenRomeijn\ProjectsRest\Repository\Mapper;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ProjectMapperTest extends TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testMapProjects()
+    {
+    }
+
+    public function testMapProjectsEmpty()
+    {
+    }
+
+    public function testMapProjectsNull()
+    {
+    }
+
+    public function testMapProject()
+    {
+    }
+
+    public function testMapProjectNull()
+    {
+    }
+}
