bfe278cd05e4fc2715e21d751e772a435b83f1fa
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-13 17:12:23 +0200

    Added file for test cases.

---
+++ tests/Repository/ProjectRepositoryTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Jurgen Romeijn <jurgen.romeijn@gmail.com>
+ */
+
+namespace JurgenRomeijn\ProjectsRest\Repository;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ProjectRepositoryTest extends TestCase
+{
+    public function testFindAll()
+    {
+    }
+
+    public function testFindAllNotFound()
+    {
+    }
+
+    public function testFindAllAddImages()
+    {
+    }
+
+    public function testFindAllNotFoundAddImages()
+    {
+    }
+
+    public function testFindAllAddImagesEmpty()
+    {
+    }
+
+    public function testFindAllAddImagesNull()
+    {
+    }
+}
