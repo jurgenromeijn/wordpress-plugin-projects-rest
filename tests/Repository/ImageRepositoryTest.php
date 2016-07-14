ba434ce1a28a6fccb1dfdb4f8989e165ad5b333a
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-14 08:38:50 +0200

    Added scaffolding of the ImageRepositoryTest.

---
+++ tests/Repository/ImageRepositoryTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Jurgen Romeijn <jurgen.romeijn@gmail.com>
+ */
+
+namespace JurgenRomeijn\ProjectsRest\Repository;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use WP_Post as WordPressPost;
+
+class ImageRepositoryTest extends TestCase
+{
+    public function testFindImages()
+    {
+
+    }
+
+    public function testFindImagesEmpty()
+    {
+
+    }
+
+    public function testFindImagesNull()
+    {
+
+    }
+
+    public function testFindFeaturedImages()
+    {
+
+    }
+
+    public function testFindFeaturedImagesEmpty()
+    {
+
+    }
+
+    public function testFindFeaturedImagesNull()
+    {
+
+    }
+}
