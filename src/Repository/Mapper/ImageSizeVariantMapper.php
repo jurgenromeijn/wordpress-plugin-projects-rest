1db1be92a29785bd95c4df3955c117e88c008158
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-27 08:42:47 +0200

    Normalize ImageSizeVariant name.

--- src/Repository/Mapper/ImageSizeVariantMapper.php
+++ src/Repository/Mapper/ImageSizeVariantMapper.php
@@ -33,7 +33,8 @@ class ImageSizeVariantMapper implements ImageSizeVariantMapperInterface
         $variants = ArrayHelper::findArray(self::META_SIZES, $metaData);
         foreach ($variants as $variantName => $variantMetaData) {
             if (is_string($variantName) and !empty($variantMetaData)) {
-                $imageSizeVariants[$variantName] = $this->mapImageSizeVariant($image, $variantMetaData);
+                $normalizedVariantName = $this->getNormalizedVariantName($variantName);
+                $imageSizeVariants[$normalizedVariantName] = $this->mapImageSizeVariant($image, $variantMetaData);
             }
         }
 
@@ -74,5 +75,10 @@ class ImageSizeVariantMapper implements ImageSizeVariantMapperInterface
             $url = str_replace($oldFileName, $fileName, $imageUrl);
         }
         return $url;
+    }
+
+    private function getNormalizedVariantName($variantName)
+    {
+        return str_replace('-', '_', $variantName);
     }
 }
